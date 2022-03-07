66b98d33a88e00eb700423e97a8b0a7b601f7da3
eunsol-an <ae__sol@naver.com>
2022-03-07 18:56:20 +0900

    initial commit

---
+++ src/Gugudan.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		// System.out.println();을 이용해서 직접 계산을 입력하여 화면에 출력
+		
+		// 2단
+		System.out.println("2단");
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		
+		// 3단
+		System.out.println("3단");
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	}
+}
