37b2644b6cfa404337aefdf0a77ad6a21c9c87cf
eunsol-an <ae__sol@naver.com>
2022-03-07 22:25:50 +0900

    feat: 6단 추가
    반복문 while문을 이용한 6단 추가

---
+++ src/Gugudan_6dan.java
@@ -0,0 +1,18 @@
+
+public class Gugudan_6dan {
+
+	public static void main(String[] args) {
+		// 반복문 while문 이용하기
+		
+		// 6단
+		System.out.println("6단");
+		
+		int i = 1;
+		while (i < 10) {
+			System.out.println(6 * i); // 곱셈 결과 출력
+			i++; // i값을 1씩 증가
+		}
+
+	}
+
+}
