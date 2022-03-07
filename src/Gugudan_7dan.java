d16f97c49d383fc5021be218c900eb4c2a18fb6a
eunsol-an <ae__sol@naver.com>
2022-03-07 22:26:08 +0900

    feat: 7단 추가
    반복문 for문을 이용한 7단 추가

---
+++ src/Gugudan_7dan.java
@@ -0,0 +1,16 @@
+
+public class Gugudan_7dan {
+
+	public static void main(String[] args) {
+		// 반복문 for문 이용하기
+		
+		// 7단
+		System.out.println("7단");
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.println(7 * i); // 곱셈 결과 출력
+		}
+
+	}
+
+}
