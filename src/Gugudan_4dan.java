741c06e58a703ed4973dc0d2dffa5a9979f5b2cb
eunsol-an <ae__sol@naver.com>
2022-03-07 19:17:11 +0900

    feat : 4단 추가
    변수를 도입한 4단 추가

---
+++ src/Gugudan_4dan.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Gugudan_4dan {
+
+	public static void main(String[] args) {
+		// 4단을 구현하는 경우, 4라는 값은 변하지 않는다. 4를 변수로 만든다!
+		
+		// 4단
+		System.out.println("4단");
+
+		int number = 4; // 4를 변수 number에 저장
+		int result = number * 1;
+		System.out.println(result);
+		result = number * 2;
+		System.out.println(result);
+		result = number * 3;
+		System.out.println(result);
+		result = number * 4;
+		System.out.println(result);
+		result = number * 5;
+		System.out.println(result);
+		result = number * 6;
+		System.out.println(result);
+		result = number * 7;
+		System.out.println(result);
+		result = number * 8;
+		System.out.println(result);
+		result = number * 9;
+		System.out.println(result);
+	}
+
+}
