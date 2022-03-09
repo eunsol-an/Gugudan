f35f5e9f02f690123f84a81ced9e0b8017ba8d96
eunsol-an <ae__sol@naver.com>
2022-03-09 23:02:43 +0900

    refactor: Gugudan_Array 배열에 저장, 출력 기능 분리
    이후 method부분의 이해를 돕기 위해 배열에 저장, 출력 기능 분리

--- src/Gugudan_Array.java
+++ src/Gugudan_Array.java
@@ -3,15 +3,20 @@ public class Gugudan_Array {
 	public static void main(String[] args) {
 		// 배열 Array를 이용하여 2단 출력하기
 		
+		
+		// 구구단 실행결과를 배열에 저장
 		int[] result = new int[9];
 		for(int i=0; i<result.length; i++) {
 			result[i] = 2 * (i + 1);
+		}
+		
+		// 구구단 실행결과 출력
+		for(int i=0; i<result.length; i++) {
 			System.out.println(result[i]);
 		}
 
 
 		// 이중for문을 이용하여 2단부터 9단까지 출력하기
-		
 		int[] gugudan = new int[9];
 		for (int i=1; i<gugudan.length; i++) {
 			int num = i + 1;
